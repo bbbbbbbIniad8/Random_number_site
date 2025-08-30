ac8b7cb0ae6c4af8a4ad35b612fbfc8896445fae
0x7b <0x7b123@gmail.com>
2025-08-31 00:51:04 +0900

    削除機能のための関数実装

--- index.js
+++ index.js
@@ -1,46 +1,58 @@
 let intervalId = null;
 let first = true;
-let rotatecnt = 0;
-let elementcnt = 0;
-let rolllst = [];
-const d = new Date();
-const date = d;
-const seconds = d.getSeconds() % 10;
-document.getElementById("date").textContent = `SL ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+let rotateCnt = 0;
+let elementCnt = 0;
+let rollLst = [];
+const delateWord = "D"
+const date = new Date();
+const seconds = date.getSeconds() % 10;
+document.getElementById("date").textContent = `SL ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 const slot = document.getElementById("slot");
 
-function lst_criate(){
+function lstCreate(){
     if(document.getElementById('inputed').value === ""){
         return;
     }
-
     const newlstElement = document.createElement('div');
-    newlstElement.id = `text${elementcnt}`;
+    newlstElement.id = `text${elementCnt}`;
     newlstElement.className = `roll`;
     newlstElement.textContent = `${document.getElementById('inputed').value}`;
     document.getElementById('columns').appendChild(newlstElement);
     document.getElementById('inputed').value = "";
-    elementcnt++;
+    elementCnt++;
+}
+
+function elementDelete(index){
+    rollLst[index] = delateWord;
+}
+
+function extractElment(lst, cnt, delateWord){
+    let target = 0;
+    const len = lst.length;
+    while(lst[(cnt+target)%len] == delateWord){
+        target++;
+    }
+    return lst[(cnt+target)%len]
 }
 
 function update() {
-    rotatecnt++;
-    slot.textContent = (rolllst[rotatecnt % rolllst.length]);
+    rotateCnt++;
+    slot.textContent = extractElment(rollLst, rotateCnt, delateWord)
 }
 
-function read_input(){
-    rolllst = [];
-    for(let i = 0;i < elementcnt;i++){
+function readInput(){
+    rollLst = [];
+    for(let i = 0;i < elementCnt;i++){
         const target = document.getElementById(`text${i}`).textContent;
-        rolllst.push(target);
+        rollLst.push(target);
     }
 }
 
 function toggle() {
     let btntext = document.getElementById("controlBtnText");
     if (intervalId === null) {
-        read_input()
-        if(rolllst.length <= 0){
+        readInput()
+        if(rollLst.length <= 0){
             alert('要素を入力してください!!');
             return;
         }
@@ -49,7 +61,7 @@ function toggle() {
     } else {
         clearInterval(intervalId);
         if (first === true){
-            slot.textContent = (rolllst[seconds % rolllst.length]);
+            slot.textContent = extractElment(rollLst, seconds, delateWord);
             first = false;
         }
         btntext.textContent= "start";
